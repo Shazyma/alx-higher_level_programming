a765b21cff6e2d7cb66bf7ba4867c02bef5fc474
Shazyma <sharontoo015@gmail.com>
2024-04-30 04:24:15 -0700

    net1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-"""
-Python script that takes 2 arguments in order to solve the given challenge
-"""
+"""Fetches https://alx-intranet.hbtn.io/status."""
 import requests
-from sys import argv
 
-if __name__ == '__main__':
-    url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
-    r = requests.get(url)
-    commits = r.json()
-    for commit in commits[:10]:
-        print(commit.get('sha'), end=': ')
-        print(commit.get('commit').get('author').get('name'))
+
+if __name__ == "__main__":
+    r = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
